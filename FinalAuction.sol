0b65a67b5ffac3027a8e06d65d95523214be47f1
licpmb <licpmb@gmail.com>
2024-11-17 12:37:09 -0300

    finalAuction solution

--- FinalAuction.sol
+++ FinalAuction.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ScrollSepoliaAuction {
+contract FinalAuction {
     address public owner;
     address public seller;
     address public highestBidder;
