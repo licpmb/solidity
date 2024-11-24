e73c288e33e747793572a6555c7c79c3bdd5b383
licpmb <licpmb@gmail.com>
2024-11-24 17:39:41 -0300

    +SimpleDEX ok

--- PruebasTokenDEX.sol
+++ PruebasTokenDEX.sol
@@ -1,4 +1,124 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenA is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("TokenA", "TKA")
+        Ownable(initialOwner)
+        ERC20Permit("TokenA")
+    {
+        _mint(msg.sender, 5000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
+
+
+
+/*SimpleDex Ejemplo 
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/// @author [Salvador Piccolini]
+contract SimpleDEX is Ownable {
+    /// Token A 
+    IERC20 public tokenA;
+
+    /// Token B 
+    IERC20 public tokenB;
+
+    ///  The amount of token A added
+    ///  The amount of token B added
+    event LiquidityAdded(uint256 amountA, uint256 amountB);
+
+    /// Emitted when liquidity is removed
+    event LiquidityRemoved(uint256 amountA, uint256 amountB);
+
+    /// The address of the user performing the swap
+    event TokenSwapped(address indexed user, uint256 amountIn, uint256 amountOut);
+
+    /// Initializes the DEX with two tokens
+    constructor(address _tokenA, address _tokenB) Ownable(msg.sender) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        
+    }
+
+    /// Adds liquidity to the pool
+    function addLiquidity(uint256 amountA, uint256 amountB) external onlyOwner {
+        require(amountA > 0 && amountB > 0, "Amounts must be > 0");
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        emit LiquidityAdded(amountA, amountB);
+    }
+
+    /// Removes liquidity from the pool
+    function removeLiquidity(uint256 amountA, uint256 amountB) external onlyOwner {
+        require(amountA <= tokenA.balanceOf(address(this)) && amountB <= tokenB.balanceOf(address(this)), "Low liquidity");
+
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+
+        emit LiquidityRemoved(amountA, amountB);
+    }
+
+    /// Swaps token A for token B
+    function swapAforB(uint256 amountAIn) external {
+        require(amountAIn > 0, "Amount must be > 0");
+
+        uint256 amountBOut = getAmountOut(amountAIn, tokenA.balanceOf(address(this)), tokenB.balanceOf(address(this)));
+
+        tokenA.transferFrom(msg.sender, address(this), amountAIn);
+        tokenB.transfer(msg.sender, amountBOut);
+
+        emit TokenSwapped(msg.sender, amountAIn, amountBOut);
+    }
+
+    /// Swaps token B for token A
+    function swapBforA(uint256 amountBIn) external {
+        require(amountBIn > 0, "Amount must be > 0");
+
+        uint256 amountAOut = getAmountOut(amountBIn, tokenB.balanceOf(address(this)), tokenA.balanceOf(address(this)));
+
+        tokenB.transferFrom(msg.sender, address(this), amountBIn);
+        tokenA.transfer(msg.sender, amountAOut);
+
+        emit TokenSwapped(msg.sender, amountBIn, amountAOut);
+    }
+
+    /// Gets the price of a token relative to the other
+    function getPrice(address _token) external view returns (uint256) {
+        require(_token == address(tokenA) || _token == address(tokenB), "Invalid token");
+
+        return _token == address(tokenA)
+            ? (tokenB.balanceOf(address(this)) * 1e18) / tokenA.balanceOf(address(this))
+            : (tokenA.balanceOf(address(this)) * 1e18) / tokenB.balanceOf(address(this));
+    }
+
+    /// Calculates the output amount of a swap
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) private pure returns (uint256) {
+        return (amountIn * reserveOut) / (reserveIn + amountIn);
+    }
+}
+
+*/
+
+/*SimpleDEX Ejemplo 2
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -102,3 +222,5 @@ contract SimpleDEX {
         return reserveOut - newReserveOut;
     }
 }
+
+*/
